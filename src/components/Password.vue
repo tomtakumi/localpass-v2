abbb6a829554ed6615295928aa1c875dececabb1
Tom Takumi V. Okamoto Jr <ttvokamoto@gmail.com>
2018-03-29 00:22:27 +0800

    3/29 12:22am

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>  
+  <div class="list">  
     <v-subheader class="orange--text">{{ password.group }}</v-subheader>
 
     <v-list-group 
@@ -7,10 +7,10 @@
       v-model="item.model" 
       :key="item._id"      
       :prepend-icon="item.model ? iconup : icondown"
-      append-icon=""
+      append-icon="" no-action
       >
 
-      <v-list-tile slot="activator" class="px-0">                       
+      <v-list-tile slot="activator">                       
         <v-list-tile-content>
           <v-list-tile-title class="orange--text text--lighten-1">{{ item.system }}</v-list-tile-title>
           <v-list-tile-sub-title>system</v-list-tile-sub-title>
@@ -18,30 +18,30 @@
       </v-list-tile>
 
       <v-list-tile :ripple="{ class: 'orange--text' }" avatar @click="copyUser(item.user_name)">
-        <v-list-tile-avatar>
-          <!--<v-icon color="orange lighten-2">lock_outline</v-icon>-->
-        </v-list-tile-avatar>
+        <!--<v-list-tile-avatar>
+          <v-icon color="orange lighten-2">lock_outline</v-icon>
+        </v-list-tile-avatar>-->
         <v-list-tile-content>
           <v-list-tile-title class="orange--text text--lighten-2">{{ item.user_name }}</v-list-tile-title>
           <v-list-tile-sub-title>User name</v-list-tile-sub-title>
         </v-list-tile-content>
-        <!--<v-list-tile-action @click.stop>
-          <v-btn icon @click="edit()">
-            <v-icon color="orange lighten-2" small>edit</v-icon>
-          </v-btn>
-        </v-list-tile-action>-->
       </v-list-tile>
 
       <v-list-tile :ripple="{ class: 'orange--text' }" avatar @click="copyPass(item.pass)">
-        <v-list-tile-avatar>
-          <!--<v-icon color="orange lighten-2">lock_open</v-icon>-->
-        </v-list-tile-avatar>
+        <!--<v-list-tile-avatar>
+          <v-icon color="orange lighten-2">lock_open</v-icon>
+        </v-list-tile-avatar>-->
         <v-list-tile-content>
           <v-list-tile-title class="orange--text text--lighten-2">
-            {{ passNotVissible( item.pass ) }}
+            {{ passNotVissible( item.pass, toggleVisibility ) }}
           </v-list-tile-title>
           <v-list-tile-sub-title>Password</v-list-tile-sub-title>
         </v-list-tile-content>
+        <v-list-tile-action @click.stop>
+          <v-btn icon @click="showPassword()">
+            <v-icon color="orange lighten-2" small>{{ visibilityIcon }}</v-icon>
+          </v-btn>
+        </v-list-tile-action>
       </v-list-tile> 
 
     </v-list-group>  
@@ -81,15 +81,23 @@ export default {
       snackbar: false,
       msgUserCopied: "User name copied.",
       msgPassCopied: "Password copied.",
-      snackbarText: ""
+      snackbarText: "",
+      toggleVisibility: false,
+      visibilityIcon: 'visibility'
     };
   },
   computed: {
     
   },
   methods: {
-    edit() {
-      this.$router.push({ path: '/form' })
+    showPassword() {
+      if (this.visibilityIcon === 'visibility') {
+        this.visibilityIcon = 'visibility_off'
+        this.toggleVisibility = true
+      } else {
+        this.visibilityIcon = 'visibility'
+        this.toggleVisibility = false
+      }
     },
     copyPass(value) {
       this.$clipboard(value);
@@ -101,8 +109,16 @@ export default {
       this.snackbarText = this.msgUserCopied;
       this.snackbar = true;
     },
-    passNotVissible(pass) {
-      return `*`.repeat(pass.length)
+    passNotVissible(pass, toggleVisibility) {
+      let pwd 
+
+      if (toggleVisibility) {
+        pwd = pass        
+      } else {
+        pwd = `•`.repeat(pass.length)
+      }
+      
+      return pwd
     }
   }
 };
